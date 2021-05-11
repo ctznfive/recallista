77809c15be760e7099336add0a1a5db2f2d57152
ctznfive <ctznfive@gmail.com>
2021-05-11 18:35:39 +0300

    Added domain classes

---
+++ myrecall/src/test/java/microservices/app/myrecall/user/User.java
@@ -0,0 +1,15 @@
+package microservices.app.myrecall.user;
+
+import lombok.*;
+
+/**
+ * This class stores information to identify the user.
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String alias;
+}
