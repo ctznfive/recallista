d7ce46dc53f46423e84cd74b2106b426b6f58a13
ctznfive <ctznfive@gmail.com>
2021-06-25 15:23:26 +0300

    updated REST challenge controller

--- recallista/src/main/java/microservices/app/recallista/challenge/ChallengeController.java
+++ recallista/src/main/java/microservices/app/recallista/challenge/ChallengeController.java
@@ -2,7 +2,9 @@ package microservices.app.recallista.challenge;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This class implements a REST API to get random challenges
