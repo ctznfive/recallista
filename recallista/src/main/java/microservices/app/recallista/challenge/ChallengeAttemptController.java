8fc9cb82285b7e296f0af8efaead0a2e1d8b7969
ctznfive <ctznfive@gmail.com>
2021-10-15 16:47:47 +0300

    Adding the working implementation to ChallengeAttemptController

--- recallista/src/main/java/microservices/app/recallista/challenge/ChallengeAttemptController.java
+++ recallista/src/main/java/microservices/app/recallista/challenge/ChallengeAttemptController.java
@@ -2,6 +2,9 @@ package microservices.app.recallista.challenge;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChallengeAttemptController {
 
     private final ChallengeService challengeService;
+
+    @PostMapping
+    ResponseEntity<ChallengeAttempt> postResult(@RequestBody ChallengeAttemptDTO challengeAttemptDTO) {
+        return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDTO));
+    }
 }
