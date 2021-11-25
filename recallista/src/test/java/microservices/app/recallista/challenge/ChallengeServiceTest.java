80ab846fb9d6f38e38744722176193a366100cef
ctznfive <ctznfive@gmail.com>
2021-11-25 19:47:42 +0300

    Verified repositories in ChallengeServiceTest

--- recallista/src/test/java/microservices/app/recallista/challenge/ChallengeServiceTest.java
+++ recallista/src/test/java/microservices/app/recallista/challenge/ChallengeServiceTest.java
@@ -1,22 +1,41 @@
 package microservices.app.recallista.challenge;
 
+import microservices.app.recallista.user.User;
+import microservices.app.recallista.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class ChallengeServiceTest {
 
     private ChallengeService challengeService;
 
+    @Mock
+    private ChallengeAttemptRepository attemptRepository;
+    @Mock
+    private UserRepository userRepository;
+
     @BeforeEach
     public void setUp() {
-        challengeService = new ChallengeServiceImpl();
-    }
+        challengeService = new ChallengeServiceImpl(userRepository, attemptRepository);
+    };
 
     @Test
     public void checkCorrectAttemptTest() {
         // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
         ChallengeAttemptDTO attemptDTO =
                 new ChallengeAttemptDTO(50, 60, "john_doe", 3000);
 
@@ -25,11 +44,14 @@ public class ChallengeServiceTest {
 
         // then
         then(resultAttempt.isCorrect()).isTrue();
+        verify(userRepository).save(new User("john_doe"));
+        verify(attemptRepository).save(resultAttempt);
     }
 
     @Test
-    public void checkWrongAttempt() {
+    public void checkWrongAttemptTest() {
         // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
         ChallengeAttemptDTO attemptDTO =
                 new ChallengeAttemptDTO(50, 60, "john_doe", 4000);
 
@@ -38,5 +60,28 @@ public class ChallengeServiceTest {
 
         // then
         then(resultAttempt.isCorrect()).isFalse();
+        verify(userRepository).save(new User("john_doe"));
+        verify(attemptRepository).save(resultAttempt);
+    }
+
+    @Test
+    public void checkExistingUserTest() {
+        // given
+        given(attemptRepository.save(any())).will(returnsFirstArg());
+        User existingUser = new User(1L, "john_doe");
+        given(userRepository.findByAlias("john_doe"))
+                .willReturn(Optional.of(existingUser));
+        ChallengeAttemptDTO attemptDTO =
+                new ChallengeAttemptDTO(50, 60, "john_doe", 5000);
+
+        // when
+        ChallengeAttempt resultAttempt =
+                challengeService.verifyAttempt(attemptDTO);
+
+        // then
+        then(resultAttempt.isCorrect()).isFalse();
+        then(resultAttempt.getUser()).isEqualTo(existingUser);
+        verify(userRepository, never()).save(any());
+        verify(attemptRepository).save(resultAttempt);
     }
 }
